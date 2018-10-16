59853c9981f94301f73735170bc4241e4337083f
KERELLL <kirillkirill2016@mail.ru>
2018-10-16 11:26:00 +0300

    new dishes and func

--- Project/app/src/main/java/com/example/kirillrychkov/project/UserSetting.java
+++ Project/app/src/main/java/com/example/kirillrychkov/project/UserSetting.java
@@ -3,6 +3,7 @@ package com.example.kirillrychkov.project;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
@@ -31,6 +32,7 @@ public class UserSetting extends AppCompatActivity {
 //        user = radioButton.getText().toString();
         if (radioGroup.getCheckedRadioButtonId() == -1)
         {
+
         }
         else
         {
